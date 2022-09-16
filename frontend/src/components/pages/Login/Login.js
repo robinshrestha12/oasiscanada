2fefbcd1442ed330bb8b1f5e36d1c782ef15e598
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-09-16 16:43:10 -0400

    render users

--- frontend/src/components/pages/Login/Login.js
+++ frontend/src/components/pages/Login/Login.js
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../Registration/Registration.css';
 import { useHistory } from "react-router-dom";
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+
 
 function Login() {
+  
   let history = useHistory();
+  const [users, setUsers] = useState([]);
+  const [useremail, setUseremail] = useState("");
+  console.log(users);
+  const fetchData =() =>{
+    const userApi = `http://localhost:8080/users`;
+    axios.get(userApi)
+    .then(function(res){
+      console.log(res.data);
+      setUsers([...res.data]);
+      //console.log(users);
+    })
+
+  }
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  function addUser(){
+    const filtered_user_table = users.filter(user => {
+      return user.useremail = useremail;
+    })
+    if(filtered_user_table.length>0){
+      console.log("user exists");
+      return;
+    }
+  }
   return (
     <div className="form">
       <div className="form-body">
-    
+      <form onSubmit={e => e.preventDefault()}>
         <div className="username">
-          <label className="form_label" for="username">User Name</label>
-          <input className="form_input" type="text" id="username" placeholder="username" />
+          <label className="form_label" for="username">User Email</label>
+          <input className="form_input" type="text" id="username" placeholder="example@example.com" />
         </div>
        
         <div className="password">
           <label className="form_label" for="password">Password</label>
           <input className="form_input" type="text" id="firstName" placeholder="Password" />
         </div>
-     
+        </form>
       </div>
+      
       <div className="footer">
         <button type="submit" className="btn1">Login</button>
         <button type="submit" className="btn2" onClick={() => history.goBack()}>Cancel</button>
