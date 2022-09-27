2366d800f9f5810f6d9c7546667998e46ee99ce2
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-09-26 23:27:53 -0400

    card test

--- frontend/src/components/pages/Testimonials/Testimonials.js
+++ frontend/src/components/pages/Testimonials/Testimonials.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Testimonials.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 export default function testimonials(){
 
   return (
@@ -53,6 +56,17 @@ export default function testimonials(){
       <p className="artist"  >To embed a YouTube video into a React app, we can add an iframe into a React component with the embed video URL as the value of the src prop.</p>
       </div>
       </div>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
     </div>
   )
 }
