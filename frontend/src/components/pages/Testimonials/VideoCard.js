90c73442d34aab0ac92ab1a0a8026f8f502e42ca
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-09-28 23:02:14 -0400

    Video Card Sample

---
+++ frontend/src/components/pages/Testimonials/VideoCard.js
@@ -0,0 +1,21 @@
+const VideoCard = ({image, title, channel, channelImage}) => {
+  return (
+    <div className='videocard'>
+          <img className='videocard__image' src={image} alt='' />
+          <div className="videocard__info">
+            <Avatar 
+              className='videocard__avatar' 
+              alt={channel} 
+              src={channelImage} 
+            />
+            <div className="videocard__text">
+              <h4>{title}</h4>
+              <p>{channel}</p>
+              <p>{views} views • {timestamp}</p>
+            </div> 
+          </div>
+        </div>
+    )
+}
+
+export default VideoCard;
